1e1ac4ad43d41876ed3fd602817d2329ce231054
lukadyc <lukaszd91@gmail.com>
2015-04-27 13:17:58 +0200

    v1

--- R/Dyczka.R
+++ R/Dyczka.R
@@ -1,1 +1,26 @@
-z=3
+
+source("http://bioconductor.org/biocLite.R")
+biocLite(c("Biobase"))
+require("Biobase")
+
+data<-read.table("datasetA_scans.txt",header=T,sep="\t")
+require("affy")
+
+data=data[c(1:17,211:227),]
+opis=read.AnnotatedDataFrame("datasetA_scans.txt",header=T,sep="\t",row.names=4,stringsAsFactors=F)
+
+opis=opis[c(1:17,211:227)]
+sampleNames(opis)=paste(sampleNames(opis),".CEL",sep="")
+
+data_Affy=ReadAffy(filenames=sampleNames(opis),verbose=T)
+
+data_Affy@cdfName=paste("ga",data_Affy@cdfName,sep="")
+data_Affy@annotation=paste("ga",data_Affy@annotation,sep="")
+
+RMA=rma(data_Affy)
+dataRMA=exprs(RMA)
+biocLite('gahgu95av2.db')
+require(gahgu95av2.db)
+experiment=new("MIAME",name="DATA",lab="IO",title="DANE")
+ExprSet=new("ExpressionSet", expr=dataRMA,phenoData=opis, experimentData=experiment,annotation="gahgu95av2.db")
+
